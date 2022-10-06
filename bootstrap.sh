dfd82a8a53382b156551662f4179a32f1ba35762
Brian Cowdery <brian@symend.com>
2022-10-06 10:56:27 -0600

    tweaked probes for unifi and plex

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,26 +14,25 @@ export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
 snap install helm --classic
 
 # Install NFS provisioner
-# https://github.com/kubernetes-sigs/nfs-subdir-external-provisioner/blob/master/charts/nfs-subdir-external-provisioner/README.md
-
 helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
 helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
     --set nfs.server=192.168.1.2 \
     --set nfs.path=/volume1/nfs-storage/
-
-# Install Rancher
-#kubectl create namespace cattle-system
-
-#helm repo add rancher-latest https://releases.rancher.com/server-charts/latest
-#helm repo update
-
-#helm install rancher rancher-latest/rancher --namespace cattle-system --set ingress.tls.source=rancher --set hostname=rancher.lan
 
 # Install MetalLB
 # TODO: Helm or manifest? Helm appeared buggy... didn't use the metallb-system namespace
 kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
 kubectl apply -f root/metallb/bgppeer.yaml
 
+# Media
+kubectl apply -f media/namespace.yaml
+kubectl apply -f media/volumes.yaml
+
+kubectl apply -f media/nzbget/nzbget.yaml
+
+kubectl apply -f media/plex/plex-claim-token.yaml
+kubectl apply -f media/plex/plex.yaml
+
 # Unifi
 kubectl apply -f unifi/namespace.yaml
-kubectl apply -f unifi/unifi-controller.yaml
+kubectl apply -f unifi/unifi-controller/unifi-controller.yaml
