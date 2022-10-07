2b5b9f1a13aa161fe991ed756ef8da6b836527d3
Brian Cowdery <brian@symend.com>
2022-10-06 20:13:19 -0600

    Updated bootstrap notes

--- Readme.md
+++ Readme.md
@@ -2,6 +2,35 @@ Home Kubernetes
 ===============
 
 
+# Getting Started
+
+Install k8s & Helm
+
+```shell
+# install k8s
+curl -sfL https://get.k3s.io | sh -s - server  \
+    --cluster-init \
+    --disable servicelb \
+    --disable traefik
+
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+
+export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
+
+# install helm
+snap install help --classic
+
+# install nfs provisioner
+helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
+helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
+    --set nfs.server=192.168.1.2 \
+    --set nfs.path=/volume1/containers/
+
+# install metallb
+kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
+```
+
+Install MEtal LB
 
 
 ## Namespaces
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,38 +1,24 @@
 #!/bin/bash
 
-# Install k3s
-curl -sfL https://get.k3s.io | sh -s - server  \
-    --cluster-init \
-    --disable servicelb \
-    --disable traefik
+## 
+## System
+## 
 
-sudo chmod 644 /etc/rancher/k3s/k3s.yaml
-
-export KUBECONFIG=/etc/rancher/k3s/k3s.yaml
-
-# Install Helm
-snap install helm --classic
-
-# Install NFS provisioner
-helm repo add nfs-subdir-external-provisioner https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/
-helm install nfs-subdir-external-provisioner nfs-subdir-external-provisioner/nfs-subdir-external-provisioner \
-    --set nfs.server=192.168.1.2 \
-    --set nfs.path=/volume1/containers/
-
-# Install MetalLB
-# TODO: Helm or manifest? Helm appeared buggy... didn't use the metallb-system namespace
-kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.13.5/config/manifests/metallb-native.yaml
+# MetalLb config 
 kubectl apply -f root/metallb/bgppeer.yaml
 
-# Media
-mkdir -p /opt/nzbget/downloads
+##
+## MEDIA
+##
 
+# Shared resources
 kubectl apply -f media/namespace.yaml
 kubectl apply -f media/volumes.yaml
 
+## Plex
 kubectl apply -f media/plex/plex-claim-token.yaml
 kubectl apply -f media/plex/plex.yaml
 
-
+# Usenet Dowload tools
 kubectl apply -f media/nzbget/nzbget.yaml
 kubectl apply -f media/sonarr/sonarr.yaml
